f87699ffe2f80e2a70b134e1d925f3a8448f49da
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-07-08 17:08:49 +0900

    please freaking work

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-import express from "express";
+import express from "express"
 import path from "path"
 import morgan from "morgan"
 
 const app = express();
-const __dirname = path.resolve();
+const __dirname = path.resolve()
 
 /* ---------- App Use ---------- */
 app.use(morgan('tiny'))
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
 /* ---------- App Use ---------- */
 
 app.get('/', (req, res) => {
-  res.send('Just gonna send it');
-});
+   res.sendFile(path.join(__dirname, 'client/build/index.html'));
+})
 
-let PORT = process.env.PORT || 9000
+let PORT = process.env.PORT || 8081
 app.listen(PORT, function() {
  console.log("Server started successfully on: " + PORT);
-});
+})
