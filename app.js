bfb4c5cda246e88fbb03812a5ad74172a269ac2f
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-07-14 00:22:59 +0900

    work

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.get('/*', (req, res) => {
 })
 
 /* ----- DEV ----- */
-let port = process.env.PORT || 8080
+let port = process.env.PORT || 80
 app.listen(port, () => {
     console.log("Server started successfully on: http://localhost:" + port)
 })
