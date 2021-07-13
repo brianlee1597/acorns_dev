52faf47d65548c284361890539de4c842de86c10
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-07-13 21:17:47 +0900

    testing

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}))
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     credentials: true
+// }))
 
 app.use(session({
     secret: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
