d2c21afe40dce4257b3b13258a7d59beb98a4356
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-07-19 19:29:35 +0900

    changed carousel

--- client/src/Gifts/PinnedSection/PinnedSection.jsx
+++ client/src/Gifts/PinnedSection/PinnedSection.jsx
@@ -1,10 +1,7 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState, useEffect } from 'react'
-import Carousel from 'react-multi-carousel'
-import PinnedComponent from './PinnedComponent';
+import { FaMapPin } from 'react-icons/fa'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import WindowDimensions from '../../Hooks/WindowDimension'
 import GiftComponent from './GiftComponent';
-import CustomDot from './CustomDot';
 
 import "react-multi-carousel/lib/styles.css";
 import './PinnedSection.scss'
@@ -14,71 +11,41 @@ const PinnedSection = props => { //The whole gift bias pinned section
     if (props.userBias === "nouser") //if user is not logged in, return nothing
         return null
 
-    const [isPopulatingGiftsArray, setIsPopulatingGiftsArray] = useState(true)
-    const [allGiftsByBias] = useState([])
+    //the title above PinnedComponent changes based on bias
+    const title = props.userBias === "Aespa"? "가장 소중한 친구, MY":
+    props.userBias === "BTS"? "Army, One and Only":
+    "Blink, For You"
 
-    const getByArtist = () => { 
-        if(props.gifts.length === 0) 
-            return
-        //if gift, then prop each gift settings to GiftComponent, then push that into AllGiftsByBias
-        if(allGiftsByBias.length === 0) 
-            for(let i = 0; i < props.gifts.length; i++)
-                allGiftsByBias.push(
-                    <GiftComponent
-                        key={i}
-                        imageurl={props.gifts[i].imageurl}
-                        title={props.gifts[i].title}
-                        type={props.gifts[i].type}
-                        user={props.gifts[i].user}
-                        amountpaid={props.gifts[i].amountpaidsofar}
-                        amountneeded={props.gifts[i].amountneeded}
-                        dateremaining={props.gifts[i].dateending} />
-                )
-    }
+    // eslint-disable-next-line no-unused-vars
+    const {height, width} = WindowDimensions()
 
-    useEffect(() => {
-        getByArtist()
-        setIsPopulatingGiftsArray(false)
-    }, [])
-
-    const responsive = { //Carousel settings
-        desktop: {
-          breakpoint: { max: 3000, min: 1000 },
-          items: 3,
-          slidesToSlide: 3 
-        },
-        tablet: {
-          breakpoint: { max: 1000, min: 415 },
-          items: 2,
-          slidesToSlide: 2 
-        },
-        mobile: {
-          breakpoint: { max: 415, min: 0 },
-          items: 1,
-          slidesToSlide: 1,
-          partialVisibilityGutter: 70
-        }
-    }
-
-    if (isPopulatingGiftsArray) return null
-    else return <PinnedComponent //Return PinnedComponent with conditional props for container and gifts
-                    userBias={props.userBias} 
-                    container={
-                        allGiftsByBias.length === 0? "carousel-nogift-container":
-                        "carousel-background-container"
-                    }
-                    gifts={
-                        allGiftsByBias.length === 0? "No Gifts to Post Yet!":
-                        <Carousel 
-                        responsive={responsive} 
-                        partialVisible={true}
-                        removeArrowOnDeviceType="mobile"
-                        containerClass={"carousel"}
-                        >
-                            {allGiftsByBias}
-                        </Carousel>
-                    }
-                />
+    return ( //PinnedComponent renders its inner stuff based on the conditional props from PinnedSection
+        <>
+            <div className="giftpinned-container">
+                <h1><span><FaMapPin /></span>&nbsp;{title}</h1>
+                <hr/>
+                {props.gifts.length === 0? <div className="nogift-exists">No Gifts to Post Yet!</div>: 
+                <Swiper 
+                className="gift-exists"
+                slidesPerView={width < 415? 'auto':width < 900? 2: 3}
+                >
+                    {props.gifts.map(eachGift => {
+                        return <SwiperSlide style={{width: width < 415? "75%": "0%"}} key={eachGift.title}>
+                        <GiftComponent
+                        imageurl={eachGift.imageurl}
+                        title={eachGift.title}
+                        type={eachGift.type}
+                        user={eachGift.user}
+                        amountpaid={eachGift.amountpaidsofar}
+                        amountneeded={eachGift.amountneeded}
+                        dateremaining={eachGift.dateending} />
+                        </SwiperSlide>
+                    })}
+                </Swiper>
+                }
+            </div>
+        </>
+    )
 }
 
 export default PinnedSection
