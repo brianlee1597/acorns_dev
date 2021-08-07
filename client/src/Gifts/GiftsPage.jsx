970453a80c6e7a997c9054f152fb44ea8b26f800
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-08-07 11:50:52 +0900

    kakaopay post test

--- client/src/Gifts/GiftsPage.jsx
+++ client/src/Gifts/GiftsPage.jsx
@@ -1,12 +1,47 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+
 import PinnedSection from '../GlobalComponents/DonGiftSections/PinnedSection/PinnedSection.jsx'
 import AllSection from '../GlobalComponents/DonGiftSections/AllCategorySection/AllSection.jsx'
 import './GiftsPage.scss'
 
-const GiftsPage = props => (
-    <>
-    <PinnedSection userBias={props.userBias} componentsByBias={props.componentsByBias} pageUrl={props.pageUrl}/>
-    <AllSection userBias={props.userBias} pageUrl={props.pageUrl}/>
-    </>
-)
+const GiftsPage = props => {
+
+    const json = {
+        "cid":"TC0ONETIME",
+        "partner_order_id":"partner_order_id",
+        "partner_user_id":"partner_user_id",
+        "item_name":"초코파이",    		  
+        "quantity":"1",
+        "total_amount":"2200",
+        "vat_amount":"200",
+        "tax_free_amount":"0",
+        "approval_url":"https://developers.kakao.com/success",
+        "fail_url":"https://developers.kakao.com/fail",
+        "cancel_url":"https://developers.kakao.com/cancel"
+    }
+      
+
+    useEffect(() => {
+        axios({
+            url: 'https://kapi.kakao.com/v1/payment/ready',
+            method: 'POST',
+            headers: {
+              'Authorization': 'KakaoAK 3ebed518b0136a6bd50560b0e1e87bbb'
+            },
+            // This will urlencode the data correctly:
+            data: new URLSearchParams(json)
+        })
+        .then(data => console.log(data.data))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <>
+        <PinnedSection userBias={props.userBias} componentsByBias={props.componentsByBias} pageUrl={props.pageUrl}/>
+        <AllSection userBias={props.userBias} pageUrl={props.pageUrl}/>
+        </>
+    )
+}
 
 export default GiftsPage
